0153ac32a9bd90c3643884683b36ff2f6f97ddd1
giannischiout <giannis.chiout@gmail.com>
2023-08-01 13:13:20 +0300

    corrected error

--- src/app/_components/movies/MetaScore.jsx
+++ src/app/_components/movies/MetaScore.jsx
@@ -6,11 +6,11 @@ const MetaScore = ({ score }) => {
 
 
   return (
-    <div class="score-container">
-      <svg class="circle-container" viewBox="2 -2 28 36" xmlns="http://www.w3.org/2000/svg">
-        <circle class="circle-container__background" r="16" cx="16" cy="16"></circle>
+    <div className="score-container">
+      <svg className="circle-container" viewBox="2 -2 28 36" xmlns="http://www.w3.org/2000/svg">
+        <circle className="circle-container__background" r="16" cx="16" cy="16"></circle>
         <circle
-          class="circle-container__progress"
+          className="circle-container__progress"
           r="16"
           cx="16"
           cy="16"
